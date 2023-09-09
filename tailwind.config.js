1d448015ad3cb2265c1eca81d260efdd7baa849b
Hakizimana Clement <h.clementoert@gmail.com>
2023-09-09 20:49:44 +0200

    Finish to setup files (index.html,style-guide for colors, font and media query for 375px)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ export default {
   theme: {
     extend: {
       screens: {
-        xs: "475px",
+        xs: "375px",
+      },
+      fontFamily: {
+        "Open Sans": ["Open Sans', sans-serif"],
+        Raleway: ["Raleway', sans-serif"],
+      },
+
+      colors: {
+        primary: {
+          "dark_blue(intro and email sign up background)":
+            " hsl(217, 28%, 15%))",
+          "dark_blue(main background)": "hsl(218, 28%, 13%)",
+          "dark_blue(footer background)": " hsl(216, 53%, 9%)",
+          "dark_blue(testimonials background)": " hsl(216, 53%, 9%)",
+        },
+        accent: {
+          "Cyan (inside call-to-action gradient)": "hsl(176, 68%, 64%)",
+          "Blue (inside call-to-action gradient)": "hsl(198, 60%, 50%)",
+          "Light Red (error)": "hsl(0, 100%, 63%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+        },
       },
     },
 
