4c99e86135d810d50fb6b57a0dcbb55b99c9bcde
Hakizimana Clement <h.clementoert@gmail.com>
2023-09-09 22:47:56 +0200

    Build the header section and a responsive navbar that adapts to mobile devices.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,21 @@ export default {
         xs: "375px",
       },
       fontFamily: {
-        "Open Sans": ["Open Sans', sans-serif"],
-        Raleway: ["Raleway', sans-serif"],
+        OpenSans: ["Open-Sans, sans-serif"],
+        Raleway: ["Raleway, sans-serif"],
       },
 
       colors: {
         primary: {
-          "dark_blue(intro and email sign up background)":
-            " hsl(217, 28%, 15%))",
-          "dark_blue(main background)": "hsl(218, 28%, 13%)",
-          "dark_blue(footer background)": " hsl(216, 53%, 9%)",
-          "dark_blue(testimonials background)": " hsl(216, 53%, 9%)",
+          "intro-and-email-sign-up-background": " hsl(217, 28%, 15%))",
+          "main-background": "hsl(218, 28%, 13%)",
+          "footer-background": " hsl(216, 53%, 9%)",
+          "testimonials-background": " hsl(216, 53%, 9%)",
         },
         accent: {
-          "Cyan (inside call-to-action gradient)": "hsl(176, 68%, 64%)",
-          "Blue (inside call-to-action gradient)": "hsl(198, 60%, 50%)",
-          "Light Red (error)": "hsl(0, 100%, 63%)",
+          "Blue-(inside-call-to-action gradient)": "hsl(198, 60%, 50%",
+          "Cyan-(inside-call-to-action gradient)": "hsl(176, 68%, 64%",
+          "Light-Red-(error)": "hsl(0, 100%, 63%)",
         },
         neutral: {
           white: "hsl(0, 0%, 100%)",
